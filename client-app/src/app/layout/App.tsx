556c60ab900777e6fa561c4ee10615c7a401c509
Deva <Mac@deva-p.lan>
2020-06-19 14:08:54 -0500

    updating react

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, Fragment } from "react";
+import { Header, Icon, List, Container } from "semantic-ui-react";
+import axios from "axios";
+import { IActivity } from "../models/activity";
+import NavBar from "../../features/nav/NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []);
+
+  return (
+    <Fragment>
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard activities={activities} />
+      </Container>
+    </Fragment>
+  );
+};
+
+export default App;
